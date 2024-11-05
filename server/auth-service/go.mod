afbb531254eb7d0277c474fe59c541f643b0d2d1
AwesomeXjs <bestawes0mex@gmail.com>
2024-11-05 13:21:07 +0500

    login test

--- server/auth-service/go.mod
+++ server/auth-service/go.mod
@@ -25,9 +25,14 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/brianvoe/gofakeit v3.18.0+incompatible // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/grpc-ecosystem/grpc-opentracing v0.0.0-20180507213350-8e809c8a8645 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -35,14 +40,30 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgtype v1.14.4 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.20.4 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.55.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/rs/cors v1.11.1 // indirect
+	github.com/sony/gobreaker v1.0.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/uber/jaeger-client-go v2.30.0+incompatible // indirect
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241021214115-324edc3d5d38 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.5.1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ server/auth-service/tests/unit/controller/login_test.go
@@ -0,0 +1,165 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/AwesomeXjs/registration-service-with-checking-mail/server/auth-service/internal/controller"
+	"github.com/AwesomeXjs/registration-service-with-checking-mail/server/auth-service/internal/model"
+	"github.com/AwesomeXjs/registration-service-with-checking-mail/server/auth-service/internal/service"
+	"github.com/AwesomeXjs/registration-service-with-checking-mail/server/auth-service/internal/utils/logger"
+	"github.com/AwesomeXjs/registration-service-with-checking-mail/server/auth-service/internal/utils/validator"
+	authService "github.com/AwesomeXjs/registration-service-with-checking-mail/server/auth-service/pkg/auth_v1"
+	"github.com/AwesomeXjs/registration-service-with-checking-mail/server/auth-service/tests/unit/mocks"
+	"github.com/brianvoe/gofakeit"
+	"github.com/gojuno/minimock/v3"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLogin(t *testing.T) {
+	level := "info"
+	type IServiceMockFunc func(mc *minimock.Controller) service.IService
+	logger.Init(logger.GetCore(logger.GetAtomicLevel(&level)))
+
+	type args struct {
+		ctx context.Context
+		req *authService.LoginRequest
+	}
+
+	var (
+		ctx      = context.Background()
+		mc       = minimock.NewController(t)
+		email    = gofakeit.Email()
+		password = gofakeit.Password(true, true, true, true, false, 8)
+
+		accessToken  = gofakeit.UUID()
+		refreshToken = gofakeit.UUID()
+		userId       = gofakeit.UUID()
+
+		req = &authService.LoginRequest{
+			Email:    email,
+			Password: password,
+		}
+
+		res = &authService.LoginResponse{
+			AccessToken:  accessToken,
+			RefreshToken: refreshToken,
+			UserId:       userId,
+		}
+
+		loginRequest = &model.LoginInfo{
+			Email:    email,
+			Password: password,
+		}
+
+		loginResponse = &model.AuthResponse{
+			AccessToken:  accessToken,
+			RefreshToken: refreshToken,
+			UserID:       userId,
+		}
+
+		serviceError = fmt.Errorf("service error")
+	)
+
+	defer t.Cleanup(mc.Finish)
+
+	tests := []struct {
+		name         string                     // название теста
+		args         args                       // аргументы которые передаем в ручку Login
+		want         *authService.LoginResponse // что хотим получить из ручки Login
+		err          error
+		IServiceMock IServiceMockFunc // функция которая возвращает замоканый сервис с нужным поведением
+	}{
+		{
+			name: "success",
+			args: args{
+				ctx: ctx,
+				req: req,
+			},
+			want: res,
+			err:  nil,
+			IServiceMock: func(mc *minimock.Controller) service.IService {
+				mock := mocks.NewIServiceMock(mc)
+				// задаем поведение мока (все методы сервиса которые вызываются внутри ручки контроллера)
+				mock.LoginMock.Expect(ctx, loginRequest).Return(loginResponse, nil)
+				return mock
+			},
+		},
+		{
+			name: "service error case",
+			args: args{
+				ctx: ctx,
+				req: req,
+			},
+			want: nil,
+			err:  serviceError,
+			IServiceMock: func(mc *minimock.Controller) service.IService {
+				mock := mocks.NewIServiceMock(mc)
+				// задаем поведение мока (все методы сервиса которые вызываются внутри ручки контроллера)
+				mock.LoginMock.Expect(ctx, loginRequest).Return(nil, serviceError)
+				return mock
+			},
+		},
+		{
+			name: "invalid email",
+			args: args{
+				ctx: ctx,
+				req: &authService.LoginRequest{
+					Email:    "123",
+					Password: password,
+				},
+			},
+			want: nil,
+			err:  validator.NewValidationErrors("[\"invalid email\"]"),
+			IServiceMock: func(mc *minimock.Controller) service.IService {
+				mock := mocks.NewIServiceMock(mc)
+				return mock
+			},
+		},
+		{
+			name: "invalid password",
+			args: args{
+				ctx: ctx,
+				req: &authService.LoginRequest{
+					Email:    email,
+					Password: "123",
+				},
+			},
+			want: nil,
+			err:  validator.NewValidationErrors("[\"password length must be between 8 and 20 characters\"]"),
+			IServiceMock: func(mc *minimock.Controller) service.IService {
+				mock := mocks.NewIServiceMock(mc)
+				return mock
+			},
+		},
+		{
+			name: "invalid password and email",
+			args: args{
+				ctx: ctx,
+				req: &authService.LoginRequest{
+					Email:    "123",
+					Password: "123",
+				},
+			},
+			want: nil,
+			err:  validator.NewValidationErrors("[\"invalid email\"]", "[\"password length must be between 8 and 20 characters\"]"),
+			IServiceMock: func(mc *minimock.Controller) service.IService {
+				mock := mocks.NewIServiceMock(mc)
+				return mock
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			IServiceMock := tt.IServiceMock(mc)
+			myController := controller.New(IServiceMock)
+
+			result, err := myController.Login(tt.args.ctx, tt.args.req)
+			require.Equal(t, tt.err, err)
+			require.Equal(t, tt.want, result)
+		})
+	}
+}
